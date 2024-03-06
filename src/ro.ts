507267b22a835b18c629118a16a590cb7cb7c2c3
Bence Danko <bencejdanko@gmail.com>
2024-03-05 17:51:01 -0800

    grid template

--- src/ro.ts
+++ src/ro.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import '../public/CAR_RILLO_LOGO.jpg'
 import '../public/profile-picture.jpg'
+import '../public/gtr3.png'
 
 
 
@@ -10,13 +11,64 @@ document.querySelector<HTMLDivElement>('#ro')!.innerHTML = /*html*/`
       <h2>Terminal<h2>
     </div>
 
-    <div class="customer">'
-      
-      <img class="portrait" src="profile-picture.jpg" alt="profile picture">
-      
-      <div class="customer-details"></div>
-    </div>
+    <div class="terminal">
 
+      <div class="header">
+        header
+      </div>
+
+      <div class="info">
+        <div class="impression">
+          <div class="GuestName">John Doe</div>
+          <img class="portrait" src="profile-picture.jpg" alt="profile picture">
+        </div>  
+
+
+        <div class="customer-details">
+            <p>Info1</p>
+            <p>Info2</p>
+            <p>Info1</p>
+            <p>Info2</p>
+            <p>Info1</p>
+            <p>Info2</p>
+            <p>Info1</p>
+            <p>Info2</p>
+          </div>
+      </div>
+
+      <div class="timer">
+        timer
+      </div>
+
+      <div class="form">
+        form
+      </div>
+
+      <div class="car">
+        <img class="car-portrait" src="gtr3.png">
+      </div>
+
+      <div class="notes">
+        notes
+      </div>
+
+      <div class="parts">
+        parts
+      </div>
+
+      <div class="progress">
+        progress
+      </div>
+
+      <div class="manage">
+        manage
+      </div>
+      
+      <div class="advisor">
+        advisor
+      </div>
+
+    </div>
 
   </div>
   
