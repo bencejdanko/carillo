2b0c54b9ef5b00af9cbe71ef9efab61f7ec875bd
Bence Danko <bencejdanko@gmail.com>
2024-03-28 13:01:49 -0700

    configuring and testing mobile backend

---
+++ dev/backend/pb_migrations/1711648729_created_test.js
@@ -0,0 +1,41 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "t3keedzd89f4sw5",
+    "created": "2024-03-28 17:58:49.307Z",
+    "updated": "2024-03-28 17:58:49.307Z",
+    "name": "test",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "3ahdeero",
+        "name": "testing",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": "",
+    "viewRule": "",
+    "createRule": "",
+    "updateRule": "",
+    "deleteRule": "",
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("t3keedzd89f4sw5");
+
+  return dao.deleteCollection(collection);
+})
--- dev/mobile/App.js
+++ dev/mobile/App.js
@@ -1,56 +1,119 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Alert, Animated, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import PocketBase from 'pocketbase';
 
 export default function App() {
+
+  const pb = new PocketBase('http://127.0.0.1:8090/');
+
+  const [pbHealth, setPbHealth] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleGetTest = async () => {
+    try {
+      const res = await pb.collection('test').getOne("37t3f7exqseru0p", {isAbort: false});
+      setEmail(res.testing)
+    } catch (error) {
+      console.log(error.originalError)
+      setEmail('Error');
+    }
+    await handleGetHealth();
+  }
+
+  const handleGetHealth = async () => {
+    try {
+      const res = await pb.health.check()
+      console.log(res)
+    } catch (error) {
+      console.log(error.originalError)
+      setPbHealth('Error');
+    }
+  }
+
+
   return (
-    <View style={{
+    <SafeAreaView
+    style={{
       backgroundColor: "#191A22",
       flex: 1,
       width: "100%",
       height: "100%",
     }}>
-      <Text style={{
-        fontSize: '30',
-        paddingTop: 50,
-        paddingLeft: 10,
-      }
-      }>
-        <Text style={{ color: "white" }}>Welcome{"\n"}back to{"\n"}</Text>
-        <Text style={{ color: "red" }}>Car-Rillo.</Text>
-      </Text>
+      <View>
 
-      <Text style={
-        {
-          paddingTop: 5,
-          color: "#636464",
-          fontSize: 15,
-          paddingLeft: 10
-        }}>{"\n"}Your own digital pitstop.</Text>
 
-      <View style={{
-        padding: 20,
-        paddingLeft: 15,
-      }}>
+        <Text style={{
+          fontSize: '30',
+          paddingTop: 50,
+          paddingLeft: 10,
+        }
+        }>
+          <Text style={{ color: "white" }}>Welcome
+          {"\n"}back to{"\n"}</Text>
+          <Text style={{ color: "red" }}>Car-Rillo.</Text>
+        </Text>
 
-        <Text>Email</Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            color: "white",
-            borderRadius: 5,
-            paddingLeft: 10,
-          }}
+        <Text style={
+          {
+            paddingTop: 5,
+            color: "#636464",
+            paddingLeft: 10
+          }}>{"\n"}Your own digital pitstop.</Text>
 
-          placeholder="Enter your email here"
-        />
+        <View style={{
+          padding: 20,
+          paddingLeft: 15,
+        }}>
+
+          <Text>Email</Text>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              color: "white",
+              borderRadius: 5,
+              paddingLeft: 10,
+            }}
+
+            placeholder="Enter your email here"
+          />
+        </View>
+
+        <View style={{
+          padding: 20,
+          paddingLeft: 15,
+        }}>
+
+          <Text>Password</Text>
+          <TextInput
+            value={pbHealth}
+            onChangeText={setPbHealth}
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              color: "white",
+              borderRadius: 5,
+              paddingLeft: 10,
+            }}
+
+            placeholder="Enter your email here"
+          />
+        </View>
+
+        <StatusBar style="auto" />
+
+        <Button
+          color="red"
+          title="Get Test"
+          onPress={handleGetTest}></Button>
       </View>
-      
-      <StatusBar style="auto" />
-    </View>
+
+
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-});
