59779ce6746c3820b5501b1a01e7d53b4134bd8f
bence danko <bencejdanko@gmail.com>
2024-04-09 23:18:46 -0700

    tech board dashboard

--- dev/web/README.md
+++ dev/web/README.md
@@ -1,43 +1,38 @@
-<div align="center" style="margin: 30px;">
-<a href="https://refine.dev/">
-  <img alt="refine logo" src="https://refine.ams3.cdn.digitaloceanspaces.com/readme/refine-readme-banner.png">
-</a>
+# create-svelte
 
-</br>
-</br>
+Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
 
-<div align="center">
-    <a href="https://refine.dev">Home Page</a> |
-    <a href="https://discord.gg/refine">Discord</a> |
-    <a href="https://refine.dev/examples/">Examples</a> |
-    <a href="https://refine.dev/blog/">Blog</a> |
-    <a href="https://refine.dev/docs/">Documentation</a>
-</div>
-</div>
+## Creating a project
 
-</br>
-</br>
-
-<div align="center"><strong>Build your <a href="https://reactjs.org/">React</a>-based CRUD applications, without constraints.</strong><br>An open source, headless web application framework developed with flexibility in mind.
-
-<br />
-<br />
-
-[![Discord](https://img.shields.io/discord/837692625737613362.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/refine)
-[![Twitter Follow](https://img.shields.io/twitter/follow/refine_dev?style=social)](https://twitter.com/refine_dev)
-
-<a href="https://www.producthunt.com/posts/refine-3?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-refine&#0045;3" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=362220&theme=light&period=daily" alt="refine - 100&#0037;&#0032;open&#0032;source&#0032;React&#0032;framework&#0032;to&#0032;build&#0032;web&#0032;apps&#0032;3x&#0032;faster | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
-
-</div>
-
-## Try this example on your local
+If you're seeing this, you've probably already done this step. Congrats!
 
 ```bash
-npm create refine-app@latest -- --example starter-vite
+# create a new project in the current directory
+npm create svelte@latest
+
+# create a new project in my-app
+npm create svelte@latest my-app
 ```
 
-## Try this example on CodeSandbox
+## Developing
 
-<br/>
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
 
-[![Open starter-vite example from refine](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/embed/github/refinedev/refine/tree/master/examples/starter-vite?view=preview&theme=dark&codemirror=1)
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ dev/web/src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+    import * as Menubar from "$lib/components/ui/menubar";
+    import * as Card from "$lib/components/ui/card";
+
+    let guest_name = "John Doe";
+    let ro_opened = "4-9-2022 9:45AM";
+    let license = "123456";
+    let vin = "123456";
+    let phone_number = "123456";
+    let service_advisor = "Shirley Smith";
+    let last_service_date = "4-9-2022 9:45AM";
+    let tag_number = "123456";
+
+    let customer_info_expanded = false;
+    let digital_assistant_expanded = false;
+</script>
+
+<Menubar.Root class='m-2'>
+    <Menubar.Menu>
+        <Menubar.Trigger>File</Menubar.Trigger>
+        <Menubar.Content>
+            <Menubar.Item>Share</Menubar.Item>
+            <Menubar.Separator />
+            <Menubar.Item>Print</Menubar.Item>
+        </Menubar.Content>
+    </Menubar.Menu>
+</Menubar.Root>
+
+<Card.Root class="m-2">
+    <Button
+        class="center self-center m-2"
+        on:click={() => (customer_info_expanded = !customer_info_expanded)}
+    >
+        {customer_info_expanded ? "Show Customer Info" : "Hide Customer Info"}
+    </Button>
+
+    <Button
+        class="center self-center m-2"
+        on:click={() =>
+            (digital_assistant_expanded = !digital_assistant_expanded)}
+    >
+        {digital_assistant_expanded
+            ? "Show Digital Assistant"
+            : "Hide Digital Assistant"}
+    </Button>
+</Card.Root>
+
+{#if customer_info_expanded}
+    <Card.Root class="m-2">
+        <div class="grid grid-cols-4">
+            {#each [{ label: "Guest Name:", value: guest_name }, { label: "RO Opened:", value: ro_opened }, { label: "License:", value: license }, { label: "VIN Number:", value: vin }, { label: "Phone Number:", value: phone_number }, { label: "Service Advisor:", value: service_advisor }, { label: "Last Service Date:", value: last_service_date }, { label: "Tag Number:", value: tag_number }] as { label, value }}
+                <Card.Root class="border-none shadow-none">
+                    <Card.Content>
+                        <b>{label}</b>
+                        <p>{value}</p>
+                    </Card.Content>
+                </Card.Root>
+            {/each}
+        </div>
+    </Card.Root>
+{/if}
+
+<div class="grid grid-cols-2 gap-2 m-2">
+    <Card.Root>
+        <Card.Header>
+            <Card.Title>RO Form</Card.Title>
+            <Card.Description>RO Number:</Card.Description>
+        </Card.Header>
+
+        <Card.Root class="grid grid-cols-2 gap-2 m-4">
+            <Card.Root class="m-4 border-none shadow-none">
+                <Card.Header>
+                    <Card.Title>Service 1</Card.Title>
+                    <Card.Description>Progress:</Card.Description>
+                </Card.Header>
+            </Card.Root>
+
+            <Card.Root class="m-4 border-none shadow-none">
+                <Card.Header>
+                    <Card.Title>Service 1</Card.Title>
+                    <Card.Description>Progress:</Card.Description>
+                </Card.Header>
+            </Card.Root>
+        </Card.Root>
+    </Card.Root>
+
+    <Card.Root>
+        <Card.Root class="m-4">
+            <Card.Header>
+                <Card.Title>Service 1</Card.Title>
+                <Card.Description>Progress:</Card.Description>
+            </Card.Header>
+        </Card.Root>
+    </Card.Root>
+</div>
